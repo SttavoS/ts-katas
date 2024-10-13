30364362e1fb77afb5dec1eae52bfe96d9fe0b48
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-10-13 20:02:05 -0300

    8-square-sum: use exponentiation operator instead

--- src/8-kyu/square-sum/square-sum.ts
+++ src/8-kyu/square-sum/square-sum.ts
@@ -5,5 +5,5 @@
  **/
 
 export function squareSum(numbers: number[]): number {
-  return numbers.reduce((acc, number) => acc + Math.pow(number, 2), 0);
+  return numbers.reduce((acc, number) => acc + number ** 2, 0);
 }
