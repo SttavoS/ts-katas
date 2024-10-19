891ebd7cce5ed37bbe39b9c3f2c1519bd247e76a
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-10-19 00:56:36 -0300

    19-10-2024: finish daily kata

---
+++ src/8-kyu/check-for-factor/check-for-factor.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, test } from "vitest";
+import { checkForFactor } from "./check-for-factor.js";
+
+describe("Check for factor", () => {
+  test("is a factor of", () => {
+    expect(checkForFactor(10, 2)).toBe(true);
+    expect(checkForFactor(63, 7)).toBe(true);
+    expect(checkForFactor(2450, 5)).toBe(true);
+    expect(checkForFactor(24612, 3)).toBe(true);
+  });
+
+  test("is not a factor of", () => {
+    expect(checkForFactor(9, 2)).toBe(false);
+    expect(checkForFactor(653, 7)).toBe(false);
+    expect(checkForFactor(2453, 5)).toBe(false);
+    expect(checkForFactor(24617, 3)).toBe(false);
+  });
+});
